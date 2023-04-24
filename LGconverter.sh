7162aecce93f2cb61a91c6b5f8e1c9de0e82b0c8
Jose Benhumea <joseramosb@gmail.com>
2023-04-23 23:32:30 -0600

    En esta rama probamos varias funcionalidades nuevas. En este commit añadimos un echo que muestra el numero de videos disponibles en la ruta. 2.Se incrementa la aleatoriedad 3. Se crea una log de los videos que se han sido seleccionados, esta misma lista sirve para que no se vuelvan a copiar los mismos videos, 4. al comando ffmpeg se agrega la opcion -loglevel panic para que no muestre la salida cuando esta realizando la conversion y en su lugar solo envia el mensaje de que el video ha sido convertido

--- LGconverter.sh
+++ LGconverter.sh
@@ -3,14 +3,34 @@
 # Solicitar la ruta
 read -p "Introduce la ruta de búsqueda: " ruta
 
+# Muestra el numero de videos disponibles en la ruta especificada
+num_videos=$(find "$ruta" -type f \( -name "*.mp4" -o -name "*.mkv" -o -name "*.avi" -o -name "*.mov" -o -name "*.wmv" -o -name "*.m4v" -o -name "*.flv" -o -name "*.3gp" -o -name "*.mpg" -o -name "*.mpeg" -o -name "*.webm" -o -name "*.ogv" -o -name "*.vob" -o -name "*.mp2" -o -name "*.m2v" -o -name "*.mpv" -o -name "*.divx" -o -name "*.xvid" -o -name "*.asf" -o -name "*.qt" -o -name "*.rm" -o -name "*.rmvb" -o -name "*.ts" -o -name "*.mxf" -o -name "*.asf" -o -name "*.mpg4" -o -name "*.mpe" -o -name "*.swf" -o -name "*.drc" -o -name "*.nut" \) | wc -l)
+echo "Disponemos de  $num_videos archivos de video en la ruta $ruta."
+
+
 # Solicitar un numero de videos a convertir
 read -p "Cuantos videos quieres seleccionar:" seleccion
 
 # Buscar los archivos de video en la ruta y subcarpetas
 videos=$(find "$ruta" -type f -name "*.mp4" -o -name "*.mkv" -o -name "*.avi")
 
-# Seleccion de n cantidad de videos al azar
-selected_videos=($(echo "$videos" | shuf -n "$seleccion"))
+
+# Seleccion de n cantidad de videos al azar con mayor aleatoriedad
+while true; do
+  selected_videos=($(echo "$videos" | tr ' ' '\n' | shuf -rn $seleccion | tr '\n' ' '))
+  echo "Se han seleccionado $seleccion videos del directorio $ruta. Se guardará una lista de los videos seleccionados en $videos_path."
+  if grep -qFf <(printf '%s\n' "${selected_videos[@]}") "$HOME/seleccionDiaria.list"; then
+    echo "Algunos de los archivos seleccionados ya existen en seleccionDiaria.list. Seleccionando otros."
+  else
+    break
+  fi
+done
+
+
+# Crea una lista de los videos que se convirtieron en la secion
+echo "$(date '+%A, %B %d, %Y %r')" >> "$HOME/seleccionDiaria.list"
+echo "${selected_videos[@]/%/$'\n'}" >> "$HOME/seleccionDiaria.list"
+
 
 # Directorio donde se guardaran los videos codificados
 videos_path="$HOME/LGtelevision"
@@ -27,12 +47,16 @@ if [[ -d "$videos_path" ]] && [[ "$(ls -A "$videos_path")" ]]; then
 fi
 
 mkdir -p "$videos_path"
+# Funcion de conversion de video a un formato que soprte el dispositivo LG HB806
+
 i=1
 for video in "${selected_videos[@]}"; do
-  output_name="LGtelevision$(printf '%04d' "$i").mp4"
-  ffmpeg -i "$video" -c:v libx264 -crf 20 -preset slow -vf "scale=-2:720,setsar=1" -c:a aac -b:a 128k -ac 2 "$videos_path/$output_name"
+  #La siguiente linea renombra el archivo de salida a la cadena LGtelevisiony una cadena de 4 caracteres consecutivos
+  output_name="$(basename "$video")"
+  ffmpeg -i "$video" -c:v libx264 -crf 20 -preset slow -vf "scale=-2:720,setsar=1" -c:a aac -b:a 128k -ac 2 -loglevel panic "$videos_path/$output_name"
   echo "Convertido $output_name"
   ((i++))
 done
 
 echo "Proceso finalizado."
+
